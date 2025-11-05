edd9e3bf62525bed1691ea7f2c22d917af4583b5
Khang-kh <nguyenthanhkhang0603@gmail.com>
2025-11-05 14:00:44 +0700

    update animation vfx

---
+++ My project (2)/Assets/Khang/Script/AttackEventHandler.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class AttackEventHandler : MonoBehaviour
+{
+    private PlayerAttack playerAttack;
+
+    void Start()
+    {
+        // Tìm script PlayerAttack trên đối tượng cha (Player 1)
+        playerAttack = GetComponentInParent<PlayerAttack>();
+        if (playerAttack == null)
+        {
+            Debug.LogError("PlayerAttack script not found on the parent (Player 1)! Cannot call VFX.");
+        }
+    }
+
+    // HÀM CÔNG KHAI (PUBLIC) mà Animation Event có thể thấy
+    public void CallSpawnVFX()
+    {
+        if (playerAttack != null)
+        {
+            // Gọi hàm thật trên Player 1
+            playerAttack.SpawnAttackVFX();
+        }
+    }
+
+    // Bạn có thể thêm các hàm cầu nối khác nếu cần
+    public void CallDealDamage()
+    {
+        if (playerAttack != null)
+        {
+            playerAttack.DealDamage();
+        }
+    }
+
+    public void CallDisableHitBox()
+    {
+        if (playerAttack != null)
+        {
+            playerAttack.DisableHitBox();
+        }
+    }
+
+    public void CallSpawnDarkSaber()
+    {
+        if (playerAttack != null)
+        {
+            playerAttack.SpawnDarkSaber();
+        }
+    }    
+}
---
+++ My project (2)/Assets/Khang/Script/DarkSaber.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class DarkSaber : MonoBehaviour
+{
+    public float speed = 15f;
+    public float lifetime = 3f; //tư hủy sau 3s
+    public int damage = 10;
+
+    private Rigidbody2D rb;
+    private float direction;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, lifetime);
+    }
+
+    public void Initialize(float dir)
+    {
+        direction = dir;
+        // áp dụng vận tốc theo hướng
+        rb.linearVelocity = new Vector2(speed * -direction, 0f);
+
+        //lật phi tiêu khi cần
+        transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * direction, transform.localScale.y, transform.localScale.z);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }    
+    }
+}
+
+
+
